c60123914c69b1e437cf34cdb654566ee951ae8f
matt700395 <matt7003@korea.ac.kr>
2022-05-05 21:25:05 +0900

    seperate commads as file

--- deploy-commands.js
+++ deploy-commands.js
@@ -1,15 +1,25 @@
+const fs = require('node:fs');
 const { SlashCommandBuilder } = require('@discordjs/builders');
 const { REST } = require('@discordjs/rest');
 const { Routes } = require('discord-api-types/v9');
 const { clientId, guildId, token } = require('./config.json');
 
+
 const commands = [
-	new SlashCommandBuilder().setName('야').setDescription('호'),
-	new SlashCommandBuilder().setName('방구대장').setDescription('뿡뿡이'),
-	new SlashCommandBuilder().setName('서버').setDescription('서버 정보를 봅니다!'),
-	new SlashCommandBuilder().setName('유저').setDescription('유저 정보를 봅니다!'),
-]
-	.map(command => command.toJSON());
+	// 	new SlashCommandBuilder().setName('야').setDescription('호'),
+	// 	new SlashCommandBuilder().setName('방구대장').setDescription('뿡뿡이'),
+	// 	new SlashCommandBuilder().setName('서버').setDescription('서버 정보를 봅니다!'),
+	// 	new SlashCommandBuilder().setName('유저').setDescription('유저정보를 봅니다!'),
+		]
+		.map(command => command.toJSON());
+
+
+const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
+for (const file of commandFiles) {
+	const command = require(`./commands/${file}`);
+	commands.push(command.data.toJSON());
+}
+
 
 const rest = new REST({ version: '9' }).setToken(token);
 
--- index.js
+++ index.js
@@ -1,11 +1,21 @@
 // Require the necessary discord.js classes
-const { Client, Intents } = require('discord.js');
+const { Client, Collection, Intents } = require('discord.js');
 const { token } = require('./config.json');
-const fs = require('fs');
+const fs = require('node:fs');
 
 // Create a new client instance
 const client = new Client({ intents: [Intents.FLAGS.GUILDS] });
 // GUILDS 서버를 예전에 길드라고 불렀다고함
+
+client.commands = new Collection(); // 위에서 만든 client 객체의 메소드로 Collection 을 새로 등록한거임
+const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
+
+for (const file of commandFiles) {
+	const command = require(`./commands/${file}`);
+	// Set a new item in the Collection
+	// With the key as the command name and the value as the exported module
+	client.commands.set(command.data.name, command);
+}
 
 // When the client is ready, run this code (only once)
 client.once('ready', () => {
@@ -15,16 +25,27 @@ client.once('ready', () => {
 client.on('interactionCreate', async interaction => {
 	if (!interaction.isCommand()) return;
 
-	const { commandName } = interaction;
+	//const { commandName } = interaction;
 
-	if (commandName === '야') {
-		await interaction.reply('호우');
-	}else if(commandName === "방구대장"){
-		await interaction.reply('뿡뿡이')
-	}else if (commandName === "서버"){
-		await interaction.reply(`서버이름 : ${interaction.guild.name} \n 서버인원 : ${interaction.guild.memberCount}`)
-	} else if (commandName === '유저') {
-		await interaction.reply(`유저태그 : ${interaction.user.tag}\n유저 아이디: ${interaction.user.id}`);
+	// if (commandName === '야') {
+	// 	await interaction.reply('호우');
+	// }else if(commandName === "방구대장"){
+	// 	await interaction.reply('뿡뿡이')
+	// }else if (commandName === "서버"){
+	// 	await interaction.reply(`서버이름 : ${interaction.guild.name} \n 서버인원 : ${interaction.guild.memberCount}`)
+	// } else if (commandName === '유저') {
+	// 	await interaction.reply(`유저태그 : ${interaction.user.tag}\n유저 아이디: ${interaction.user.id}`);
+	// }
+
+	const command = client.commands.get(interaction.commandName);
+
+	if (!command) return;
+
+	try {
+		await command.execute(interaction);
+	} catch (error) {
+		console.error(error);
+		await interaction.reply({ content: 'There was an error while executing this command!', ephemeral: true });
 	}
 });
 
